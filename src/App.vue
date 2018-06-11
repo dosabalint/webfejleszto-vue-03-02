850304bbad1ed30fa9e0387848325bcb8ff6dbcd
dosabalint <dosa.balint@gmail.com>
2018-06-11 19:53:59 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <Button></Button>
+    </div>
+</template>
+
+<script>
+import Button from './components/Button.vue';
+
+export default {
+    name: 'App',
+    components: {
+        Button
+    },
+    data: function() {
+        return {
+            message: 'Ez itt már a VueJS alkalmazásunk!'
+        };
+    }
+};
+</script>
---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <button @click="ShowAlert()">
+        {{ message }}
+    </button>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            message: 'Ez meg már a mi gombunk!'
+        };
+    },
+    methods: {
+        ShowAlert() {
+            alert('Örvendetes!');
+        }
+    }
+};
+</script>
+ 
+<style lang="scss" scoped>
+button {
+    &.btn {
+        margin: 0 1rem;
+    }
+}
+</style> 
